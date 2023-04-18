63143dec760ba7800314f144db28fd86abeeb600
angelkapv81@gmail.com <angelkapv81>
2023-04-18 18:35:21 +0300

    comment delete

--- src/js/queue-service.js
+++ src/js/queue-service.js
@@ -1,23 +1,3 @@
-//приклад використання
-
-// Імпортуємо в index.js
-
-// import QueueService from './js/queue-service';
-
-// Створюємо об'єкт
-
-// myQueue = new QueueService();
-
-// myQueue.removeQueue(); // Очищуємо чергу
-// myQueue.setQueue({ id: 123, title: 'More' }); // Додаємо дані фільму в чергу
-// myQueue.setQueue({ id: 124, title: 'More' }); // Додаємо дані фільму в чергу
-// myQueue.removeFromQueue(123); // Видаляємо фільм з черги за id
-// myQueue.removeFromQueue(124); // Видаляємо фільм з черги за id
-// myQueue.setQueue({ id: 234, title: 'Іndiana Johnes' }); // Додаємо дані фільму в чергу
-// console.log(myQueue.getQueueById(235)); // Виводить в косоль за id
-// myQueue.removeFirstItemFromQueue(); // Видаляє перший елемент з черги
-// console.log(myQueue.getFirstItemFromQueue()); // Показує перший елемент в черзі
-
 // ------------------- Реалізація класу QueueService --------------------------
 
 QUEUE = 'QueueFilms';
